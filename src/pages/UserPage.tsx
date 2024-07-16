af98e4581419a4317d171adacbdb7daaf70f49c9
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-16 13:16:44 +0200

    display previous and current bookings

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,15 +2,18 @@ import { useNavigate } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import loginData from "../utils/login.json";
 import { useQuery } from "@tanstack/react-query";
-import { useRouterState } from "@tanstack/react-router";
+//import { useRouterState } from "@tanstack/react-router";
 import { BookingDetails } from "../utils/types";
+import { ViewBookingCard } from "../components/ViewBookingCard";
+import { Tab } from "@mui/material";
+import { TabList, TabContext, TabPanel } from "@mui/lab";
 
 export const UserPage = () => {
   const navigate = useNavigate();
 
-  const userId = useRouterState({
+  /*const userId = useRouterState({
     select: (s) => s.location.state.userState?.id,
-  });
+  });*/
 
   useEffect(() => {
     if (loginData.loggedIn == false) {
@@ -19,6 +22,7 @@ export const UserPage = () => {
   }, []);
 
   const [timePeriod, setTimePeriod] = useState("future");
+  const [tabValue, setTabValue] = useState("1");
 
   function getBookings() {
     return fetch(`http://localhost:8080/api/bookings/${timePeriod}/${1}`)
@@ -34,24 +38,32 @@ export const UserPage = () => {
     queryFn: getBookings,
   });
 
-  console.log(userId);
-  console.log(data);
+  const tabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTabValue(newValue);
+    if (newValue == "1") {
+      setTimePeriod("future");
+    } else {
+      setTimePeriod("past");
+    }
+  };
 
   return (
     <>
       <h1>My Page</h1>
-      <button onClick={() => setTimePeriod("past")}>Past Bookings</button>
-      <button onClick={() => setTimePeriod("future")}>Future Bookings</button>
-      {!isLoading &&
-        data.length != 0 &&
-        data.map((value: BookingDetails) => (
-          <div key={value.id} className="flex gap-10">
-            <p>{value.office.officeName}</p>
-            <p>{value.office.location}</p>
-            <p>{value.startDate}</p>
-            <p>{value.endDate}</p>
-          </div>
-        ))}
+
+      <TabContext value={tabValue}>
+        <TabList onChange={tabChange}>
+          <Tab label="Current bookings" value="1"></Tab>
+          <Tab label="Previous bookings" value="2"></Tab>
+        </TabList>
+        <TabPanel value={tabValue}>
+          {!isLoading &&
+            data.length != 0 &&
+            data.map((value: BookingDetails) => (
+              <ViewBookingCard key={value.id} bookingDetails={value} />
+            ))}
+        </TabPanel>
+      </TabContext>
     </>
   );
 };
