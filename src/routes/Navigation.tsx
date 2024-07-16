e27ef41de31f1efd191c19f607cd883677b714e7
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-16 14:00:51 +0200

    add sticky nav bar at top

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,14 +4,33 @@ import loginData from "../utils/login.json";
 export const Navigation = () => {
   return (
     <>
-      <div>
-        <Link to="/">Main</Link>
-        {loginData.loggedIn == false && <Link to="/login">Log in</Link>}
-        {loginData.loggedIn == true && (
-          <Link onClick={() => location.reload()}>Log out</Link>
-        )}
-        {loginData.loggedIn == true && <Link to="/myPage">My Page</Link>}
-      </div>
+      <nav className="w-full fixed top-0 bg-black">
+        <div className="flex justify-between p-8">
+          <Link className="no-underline text-white" to="/">
+            Office Booking Logo
+          </Link>
+
+          <div className="flex gap-8">
+            {loginData.loggedIn == true && (
+              <Link className="no-underline text-white" to="/myPage">
+                My Bookings
+              </Link>
+            )}
+            {loginData.loggedIn == false ? (
+              <Link className="no-underline text-white" to="/login">
+                Login
+              </Link>
+            ) : (
+              <Link
+                className="no-underline text-white"
+                onClick={() => location.reload()}
+              >
+                Log out
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
       <Outlet />
     </>
   );
